7756fdd3e44ab598a8d4a44eecd99d19166ef83f
Sarathlal Sarangadharan <sarathlal.sarangadharan@nhs.net>
2025-09-03 10:45:44 +0100

    TD-5150: Fixed the space issue in search term

--- Moodle-Migration-WebUI/Services/CommandService.cs
+++ Moodle-Migration-WebUI/Services/CommandService.cs
@@ -22,7 +22,16 @@ namespace Moodle_Migration_WebUI.Services
 
             // Split input into arguments
             string[] args = input.Split(' ');
+            // Logic to consider search term with multiple word
+            string[] parts = input.Split('=');
 
+            if (parts.Length == 2)  // Ensure there's exactly one equal sign
+            {
+                string key = parts[0];   // Part before equal sign
+                string[] keyArgs = parts[0].Split(' ');
+                keyArgs[2] = keyArgs[2] + "=" + parts[1];
+                args = keyArgs;
+            }
             // Call the CommandProcessor
             return  await _commandProcessor.ProcessCommand(args);
 
